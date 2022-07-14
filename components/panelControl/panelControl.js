420999bff62771cf7675ff90c19b8a9a4e2f5a67
felix fedronic <fedronicfelix@gmail.com>
2022-07-14 12:25:38 +0200

    first commit

---
+++ components/panelControl/panelControl.js
@@ -0,0 +1,14 @@
+function PanelControl(props) {
+  const { rotateX, rotateY, rotateZ } = props;
+  return (
+    <div>
+      <button type="button" onClick={rotateX}>
+        Rotate X
+      </button>
+      <button type="button" onClick={rotateY}>Rotate Y</button>
+      <button type="button" onClick={rotateZ}>Rotate Z</button>
+    </div>
+  );
+}
+
+export default PanelControl;
